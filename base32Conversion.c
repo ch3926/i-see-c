0a5ebd990210b1f2c3d11068c44dcab8cfaef909
ch3926 <ch3926@nyu.edu>
2024-02-08 21:02:34 +0100

    Fixed some bugs.

--- base32Conversion.c
+++ base32Conversion.c
@@ -7,8 +7,9 @@ int main()
     char input[] = "111011100000101011011"; // original binary input
     int length = strlen(input); // length of binary input -- will be used for calculations
     char res[32]; // where resulting base32 representation will be stored
-    char binChunks[][6] = {"00000", "00001" "00010", "00011", "00100", "00101", "00110", "00111", "01000", "01001", "01010", "01011", "01100", "01101", "01110", "01111", "10000", "10001", "10010", "10011", "10100", "10101", "10110", "10111", "11000", "11001", "11010", "11011", "11100", "11101", "11110", "11111"};
-    char base32Table[][3] ={"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
+    char formattedStr[50];
+    char binChunks[][6] = {"00000", "00001", "00010", "00011", "00100", "00101", "00110", "00111", "01000", "01001", "01010", "01011", "01100", "01101", "01110", "01111", "10000", "10001", "10010", "10011", "10100", "10101", "10110", "10111", "11000", "11001", "11010", "11011", "11100", "11101", "11110", "11111"};
+    char base32Table[][3] ={"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"};
     
     // first ensure length of bin input is divisible by 5, if not, then pad extra zeros to the start if needed.
     // make sure length of input is divisible by 5
@@ -16,7 +17,6 @@ int main()
     {
         int remainder = (length % 5);
         int zerosNeeded = (5 - remainder);
-        char formattedStr[50];
 
         // fill zeros_str with the number of zeros_needec
         for (int i = 0; i < zerosNeeded; i++)
@@ -33,7 +33,7 @@ int main()
     for(int i=0; i<(length/5); i++){
         char chunk[6];
         strncpy(chunk, formattedStr+(5*i), 4);
-        chunk[5] = '\0' // null-terminating for compatibility with printf(), strlen() etc
+        chunk[5] = '\0'; // null-terminating for compatibility with printf(), strlen() etc
     }
 
 

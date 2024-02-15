43b4f9bf4d1c783e9b8b156cad3bac588e2d5a27
ch3926 <ch3926@nyu.edu>
2024-02-15 12:28:06 +0100

    Created simple program to convert binary integers to decimal integers.

---
+++ bintoDecInts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+
+int main(){
+    char bin_input[] = "11001";
+    int length = strlen(bin_input);
+    int decimal = 0;
+
+    // loop through bin_input backwards and add corresponding power of 2 to decimal value
+    for(int i = length-1; i>=0; i--){
+        if(bin_input[i] == '1'){
+            int exp = length-i-1;
+            decimal += pow(2, (exp));
+        }
+    }
+    printf("%d\n", decimal);
+}
+

e265fa35ed4f4154080d5641e5d65ae0d86f1ec0
ch3926 <ch3926@nyu.edu>
2024-02-15 12:37:37 +0100

    Adding a while loop for repeated division by 2.

--- dectoBinInts.c
+++ dectoBinInts.c
@@ -5,4 +5,10 @@ int main(){
     int input_decimal = 25;
     char bin[32];
     int i = 0 // starting index for placing binary numbers into output array
+
+    while(input_decimal>0){
+        bin[i] = (input_decimal % 2);
+        input_decimal /= 2;
+        i++
+    }
 }

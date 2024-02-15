4d578d4d8267323c8241d7b1124ec9e53a388dff
ch3926 <ch3926@nyu.edu>
2024-02-15 13:10:16 +0100

    Testing result printing methods.

--- dectoBinInts.c
+++ dectoBinInts.c
@@ -4,11 +4,16 @@
 int main(){
     int input_decimal = 25;
     char bin[32];
-    int i = 0 // starting index for placing binary numbers into output array
+    int i = 0; // starting index for placing binary numbers into output array
 
     while(input_decimal>0){
         bin[i] = (input_decimal % 2);
-        input_decimal /= 2;
-        i++
+        input_decimal = input_decimal/ 2;
+        i++;
     }
+    for(int j = i-1; j >= 0; j--){
+        printf("%d\n", bin[j]);
+    }
+
+    printf("binary output: %s\n", bin);
 }

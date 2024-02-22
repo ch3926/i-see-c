830a51252f844786ab873a76e21f0acef6b32a94
ch3926 <ch3926@nyu.edu>
2024-02-22 12:42:01 +0100

    wrote main() function to get user input and call my_strlen().

--- my_strlen.c
+++ my_strlen.c
@@ -8,3 +8,15 @@ void my_strlen(char *str){
     printf("%d\n", length);
 
 }
+
+int main(){
+    char input[100];
+    input[sizeof(input)-1] = '\0';
+
+    printf("Enter a string: ");
+    scanf("%s", input); // Read a string input
+
+    my_strlen(input);
+    
+    return 0;
+}

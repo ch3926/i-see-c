f962d57e6a432e21554c0db1f947b645ab04ea3b
ch3926 <ch3926@nyu.edu>
2024-03-27 12:08:26 +0100

    Cleaned up Code and added comments.

--- distribution.c
+++ distribution.c
@@ -1,7 +1,10 @@
+/* Birthdates are mapped to a zodiac sign using function "getsign"
+then I get the new schools by calling "getDistribution"
+then I print out the results in the main function */
+
 #include <stdio.h>
 #include <string.h>
 
-// dates based off picture I found on google images
 void getsign(int day, int month, char* sign) {
     if (month == 1) {
         if (day >= 21) {
@@ -78,36 +81,35 @@ void getsign(int day, int month, char* sign) {
     }
 }
 
-struct Student {
-    char name[20];
-    int day;
-    int month;
-}
-void assign_school(char* sign){
+void getDistribution(char* sign){
     char* signs[15] = {"Aquarius", "Capricorn", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"};
-    int school = 0;
+    int schoolNum = 0;
+
+    printf("Student's distribution over the next 3 years.\n");
+    
     for (int i = 1; i <= 3; i++) { // show distribution for 3 years
     
         for (int j=0; j<12; j++){ //
             if (strcmp(sign, signs[j]) == 0){ 
-                school = (i + j) % 12 + 1; // rotate based on year
+                schoolNum = (i + j) % 12 + 1; // rotate based on year
                 break;
             }
         }
-        printf("Year #%d : S%d\n", i, school);
+        printf("Year #%d : S%d\n", i, schoolNum);
     }
+    printf("--------\n");
 }
 
 int main(){
     char dayMonth[8];
     int day, month;
     char sign[12];
-    printf("Enter your birthday (day/month) format: ");
+    printf("\nEnter your birthday (day/month) format: ");
     scanf("%s",dayMonth);
     //printf("%s", dayMonth);
     sscanf(dayMonth,"%d/%d", &day, &month); //use sscanf to grab and assign ints to day and month vars
     printf("Date Entered: %d/%d\n", day, month);
     getsign(day,month,sign); // get zodiac sign based on day and month
-    assign_school(sign);
+    getDistribution(sign);
     return 0;
 }
